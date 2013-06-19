e577c3f6a4ea6a2f783f67ee32ab3d176abcc40f
Tim Abell <tim@timwise.co.uk>
2013-06-19 02:00:05 +0100

    Stub step defs provided by cucumber from first run

---
+++ features/step_definitions/stepdefs.rb
@@ -0,0 +1,9 @@
+# TODO: Rename this file to something more meaningful when I know what that might be
+Given(/^product (\d+) price £(\d+)\.(\d+) is scanned$/) do |arg1, arg2, arg3|
+	  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^The total should be £(\d+)\.(\d+)$/) do |arg1, arg2|
+	  pending # express the regexp above with the code you wish you had
+end
+
