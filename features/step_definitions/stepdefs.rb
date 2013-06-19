1e37a33d7d9920d79f1ab771a4de5a8e88b0f324
Tim Abell <tim@timwise.co.uk>
2013-06-19 03:16:48 +0100

    Draft test steps for single item

--- features/step_definitions/stepdefs.rb
+++ features/step_definitions/stepdefs.rb
@@ -1,15 +1,16 @@
 # TODO: Rename this file to something more meaningful when I know what that might be
 # TODO: capture the currency symbol for L10n
 
-Given(/^product (\d+) has price £(\d+)\.(\d+)$/) do |arg1, arg2, arg3|
-	  pending "TODO: setup product"
+Given(/^product (\d+) has price £(\d+\.\d+)$/) do |product_code, price|
+	@product = Product.new(product_code, price)
 end
 
 When(/^product (\d+) is scanned$/) do |arg1|
-	  pending "TODO: add product"
+	@checkout = Checkout.new()
+	co.scan(@product)
 end
 
-Then(/^The total should be £(\d+)\.(\d+)$/) do |arg1, arg2|
-	  pending "TODO: check total price"
+Then(/^The total should be £(\d+\.\d+)$/) do |price|
+	assert_equal(@checkout.price, price)
 end
 
