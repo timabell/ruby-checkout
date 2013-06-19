f779358d9dd0e0a5858abf80ee15bbb702d93903
Tim Abell <tim@timwise.co.uk>
2013-06-19 02:30:37 +0100

    meaningful pending messages

--- features/step_definitions/stepdefs.rb
+++ features/step_definitions/stepdefs.rb
@@ -1,9 +1,11 @@
 # TODO: Rename this file to something more meaningful when I know what that might be
+# TODO: capture the currency symbol for L10n
+
 Given(/^product (\d+) price £(\d+)\.(\d+) is scanned$/) do |arg1, arg2, arg3|
-	  pending # express the regexp above with the code you wish you had
+	  pending "TODO: step for scan of product + price"
 end
 
 Then(/^The total should be £(\d+)\.(\d+)$/) do |arg1, arg2|
-	  pending # express the regexp above with the code you wish you had
+	  pending "TODO: check total price"
 end
 
