615abdc92fec993eaa4151ba9fa5b6b3f47dfc9f
Tim Abell <tim@timwise.co.uk>
2013-06-21 02:26:41 +0100

    Basic scanning and totalling working. No discounts yet.

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -4,6 +4,7 @@ class Checkout
 	def scan(product_code)
 		product = @catalogue[product_code]
 		if product then @total += product.price end
+		puts format("Scanned '%s', price £%.2f. Subtotal £%.2f", product.code, product.price, @total)
 	end
 
 	# Inject a catalogue into the checkout. This approach would need
