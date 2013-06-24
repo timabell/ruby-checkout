7ea0d5d802c314446e6895d7be344ce8d252205e
Tim Abell <tim@timwise.co.uk>
2013-06-24 04:46:04 +0100

    trying to move rules into separate class, doesn't run

--- features/step_definitions/checkout-steps.rb
+++ features/step_definitions/checkout-steps.rb
@@ -21,27 +21,8 @@ def product_from(row)
 end
 
 Given(/^these products are available:$/) do |catalogue|
-	promotions = lambda { |basket|
-		calculated_basket = basket.clone
-		hearts = basket.count { |item| item.code == "001" }
-		if hearts > 1
-			calculated_basket << Product.new( "---", "heart multi-buy discount", BigDecimal(hearts * -0.75, 2) )
-		end
-		total = calculated_basket.reduce(BigDecimal(0)) {
-			|sum, product| sum + product.price
-		}
-		# puts format(" - ** interim total price: £%.2f **", total)
-		if total > 60
-			# take 10% off, rounded down to be tight (to match
-			# numbers in the spec)
-			discount = BigDecimal(total * 0.1, 1).floor(2)
-			calculated_basket << Product.new( "---", "10% off", -discount )
-		end
-		# return a modified basket with a discount
-		calculated_basket
-	}
-
-	@checkout = Checkout.new(catalogue, &promotions)
+	promotion_rules = Promotions.new
+	@checkout = Checkout.new(catalogue, promotion_rules.apply_promotions)
 end
 
 When(/^product "(.*?)" is scanned$/) do |product_code|
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,3 +1,4 @@
 # TODO: include whole app in one go
 require 'product'
 require 'checkout'
+require 'promotions'
